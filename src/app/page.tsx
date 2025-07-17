9063ba73a4130ce777b487b94c82c9f2aceb1c22
Kailashsinh <kailashsinhrajput25@gmail.com>
2025-07-17 10:36:24 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,37 @@ import Courses from "@/components/Courses";
 import CohortHero from "@/components/Hero";
 import NotesPage from "@/components/Notes";
 import Tutorial from "@/components/Tutorial";
+import navbar from "@/components/navbar"; // Make sure Navbar is imported
 
 export default function Home() {
   return (
-   <>
-   <CohortHero />
-   <Courses />
-<NotesPage />
-<Blog />
-   <Tutorial  />
-   <Contact />
+    <>
+      <navbar />
 
-   </>
+      <div id="home" className="scroll-mt-24">
+        <CohortHero />
+      </div>
+
+      <div id="courses" className="scroll-mt-24">
+        <Courses />
+      </div>
+
+      <div id="notes" className="scroll-mt-24">
+        <NotesPage />
+      </div>
+
+      <div id="about" className="scroll-mt-24">
+        <Blog />
+      </div>
+
+      <div id="tutorial" className="scroll-mt-24">
+        <Tutorial />
+      </div>
+
+      <div id="contact" className="scroll-mt-24">
+        <Contact />
+      </div>
+    </>
   );
 }
+
